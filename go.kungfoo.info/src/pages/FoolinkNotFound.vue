8746060e308ce7d252bee5dec537bca823ea7aea
Foo JH <foojihaw@hotmail.com>
2024-11-10 17:10:37 +0700

    NEW: selective pipline parent

---
+++ go.kungfoo.info/src/pages/FoolinkNotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">
+        404
+      </div>
+
+      <div class="text-h2" style="opacity:.4">
+        This Foolink is not registered.
+      </div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="blue"
+        unelevated
+        href="http://ihazcodez.kungfoo.info"
+        label="Go Home"
+        no-caps
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'FoolinkNotFound'
+});
+</script>
